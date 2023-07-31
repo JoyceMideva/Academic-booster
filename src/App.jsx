fcd68bf8e788ac0e7e08d5f0d42c8e23e2345e91
Hawah <hawawavuda2004@gmail.com>
2023-07-31 09:29:37 +0300

    worked on the form in login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import About from "./pages/About";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 export default function App() {
   return (
     <div>
-<Home/>
+{/* <Home/> */}
+{/* <About/> */}
+<Login/>
     </div>
   )
 }
---
+++ src/components/Details.jsx
@@ -0,0 +1,53 @@
+import google from "../assets/images/google.png";
+
+function Details() {
+  return (
+    <div className="container mx-auto bg-[#3871c1] h-[100vh]">
+      <div className="">
+        <input
+          type="text"
+          placeholder="Email Address"
+          className=" justify-center p-5 w-[70%] outline-none rounded-full border-2"
+          name="email"
+        />
+      </div>
+      <div className=" my-16">
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-5 w-[70%] outline-none rounded-full border-2"
+          name="password"
+        />
+      </div>
+      <div>
+        <input type="checkbox" />
+        <label className="my-6 mx-5">Remember Me</label>
+        <button className=" text-[#45C9A1] mx-28">Forgot Password?</button>
+      </div>
+
+      <button
+        className="bg-teal-500 my-5 text-lg font-bold text-white py-4 px-2 w-[60%] rounded-full mx-16"
+        type="submit"
+      >
+        Sign In
+      </button>
+
+      <div className="flex gap-4">
+        <div className="w-[35%] border-b-2 border-gray-300"></div>
+        <p className="text-2xl my-1">OR</p>
+        <div className="w-[35%] border-b-2 border-gray-300"></div>
+      </div>
+      <div className="flex gap-6 my-12 w-[70%] justify-center">
+        <img src={google} width={50} />
+        <h2 className=" text-2xl font-bold text-blue-600 my-4">
+          Log In With Google
+        </h2>
+      </div>
+      <div className="flex justify-center w-[70%] gap-3">
+        <p className="text-2xl">Dont Have An Account Yet?</p>
+        <button className="text-[#45C9A1] text-2xl">Sign Up Free</button>
+      </div>
+    </div>
+  );
+}
+export default Details;
