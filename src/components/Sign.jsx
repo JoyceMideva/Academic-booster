ae8a4b5c6e57fb6a7465087a25b3094a08c8a1ce
Hawah <hawawavuda2004@gmail.com>
2023-07-31 16:23:16 +0300

    modified the form in signup

--- src/components/Sign.jsx
+++ src/components/Sign.jsx
@@ -1,15 +1,16 @@
-import signup from "../assets/images/bj.jpeg";
 import { BsFillEyeSlashFill } from "react-icons/bs";
 import { LiaEyeSolid } from "react-icons/lia";
 import google from "../assets/images/google.png";
+import signup from "../assets/images/login.jpeg";
+
 
 function Sign() {
   return (
     
       <div className="container mx-auto   ">
-        <div className="flex h-[100vh]">
+        <div className="flex h-[91vh]">
           <div className="flex-1 ">
-            <img src={signup} width={700}/>
+            <img src={signup}/>
           </div>
           <div className="justify-center flex-1 bg-[#3871c1]">
           <h1 className="text-4xl font-bold text-center text-white my-3">
@@ -21,7 +22,7 @@ function Sign() {
               <input
                 type="text"
                 placeholder="First Name"
-                className="w-[90%] mx-6 outline-none rounded-md border-2 p-5 my-5"
+                className="w-[90%] mx-6 outline-none rounded-md border-2 p-4 my-5"
                 name="firstName"
               />
             </div>
@@ -30,7 +31,7 @@ function Sign() {
               <input
                 type="text"
                 placeholder="Last Name"
-                className="w-[100%] outline-none rounded-md border-2 p-5 my-5"
+                className="w-[100%] outline-none rounded-md border-2 p-4 my-5"
                 name="lastName"
               />
             </div>
@@ -40,13 +41,13 @@ function Sign() {
             <input
               type="email"
               placeholder="Email Address"
-              className="outline-none rounded-md border-2 p-5 w-[70%] my-5"
+              className="outline-none rounded-md border-2 p-4 w-[70%] my-3"
               name="email"
             />
          
             
               <input
-                className="flex-1 outline-none my-5 py-4 px-2 border-2 border-grey-400 rounded-md w-[70%]"
+                className="flex-1 outline-none my-3 p-4 px-2 border-2 border-grey-400 rounded-md w-[70%]"
                 type="password"
                 placeholder="Password"
                 name="password"
@@ -57,12 +58,12 @@ function Sign() {
             <input
               type="password"
               placeholder="Confirm Password"
-              className="outline-none rounded-md border-2 p-5 w-[70%] my-5"
+              className="outline-none rounded-md border-2 p-4 w-[70%] my-3"
               name="confirmPassword"
             />
           </div>
           <button
-            className="bg-white my-5 text-lg font-bold text-[#3871c1] py-4 px-2 w-[60%] rounded-full mx-16"
+            className="bg-white my-1 text-lg font-bold text-[#3871c1] py-4 px-2 w-[60%] rounded-full mx-16"
             type="submit"
           >
             Create Account
@@ -72,7 +73,7 @@ function Sign() {
             <p className="text-2xl my-1 text-white">OR</p>
             <div className="w-[35%] border-b-2 border-gray-300"></div>
           </div>
-          <div className="flex gap-6 my-12 w-[80%] justify-center shadow-md p-3 mx-3 rounded-full">
+          <div className="flex gap-6 my-7 w-[80%] justify-center shadow-md p-3 mx-3 rounded-full">
             <img src={google} width={50} />
             <button className=" text-2xl font-bold text-white my-4">
               Sign In With Google
