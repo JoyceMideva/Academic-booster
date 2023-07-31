254ae8312adb36706182d3002f06124bdf5befa5
Hawah <hawawavuda2004@gmail.com>
2023-07-31 09:58:42 +0300

    modified the login page

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,8 +1,14 @@
 import google from "../assets/images/google.png";
+import login from "../assets/images/login.jpeg"
 
 function Details() {
   return (
-    <div className="container mx-auto bg-[#3871c1] h-[100vh]">
+    <div className="container mx-auto  h-[100vh] ">
+         <div className="flex ">
+        <div className="flex-1">
+          <img src={login} />
+        </div>
+        <div className="p-6 justify-center flex-1 bg-[#3871c1]">
       <div className="">
         <input
           type="text"
@@ -22,11 +28,11 @@ function Details() {
       <div>
         <input type="checkbox" />
         <label className="my-6 mx-5">Remember Me</label>
-        <button className=" text-[#45C9A1] mx-28">Forgot Password?</button>
+        <button className=" text-white mx-28">Forgot Password?</button>
       </div>
 
       <button
-        className="bg-teal-500 my-5 text-lg font-bold text-white py-4 px-2 w-[60%] rounded-full mx-16"
+        className=" bg-white my-5 text-lg font-bold text-[#3871c1] py-4 px-2 w-[60%] rounded-full mx-16"
         type="submit"
       >
         Sign In
@@ -37,17 +43,21 @@ function Details() {
         <p className="text-2xl my-1">OR</p>
         <div className="w-[35%] border-b-2 border-gray-300"></div>
       </div>
-      <div className="flex gap-6 my-12 w-[70%] justify-center">
+      <div className="flex gap-6  my-12 w-[70%] justify-center">
         <img src={google} width={50} />
-        <h2 className=" text-2xl font-bold text-blue-600 my-4">
+        <h2 className=" text-2xl font-bold text-white my-4">
           Log In With Google
         </h2>
       </div>
       <div className="flex justify-center w-[70%] gap-3">
         <p className="text-2xl">Dont Have An Account Yet?</p>
-        <button className="text-[#45C9A1] text-2xl">Sign Up Free</button>
+        <button className="text-white text-2xl">Sign Up</button>
       </div>
     </div>
+    
+        
+</div>
+</div>
   );
 }
 export default Details;
