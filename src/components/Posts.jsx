1cf247886871fe68b92b12a4d134d40205cb2bf5
JoyceMideva <mideva061@gmail.com>
2023-08-02 15:46:46 +0200

    worked on the add post

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,5 +1,7 @@
 import Post from "./Post";
 import munene from "../assets/images/munene.png";
+import { useContext,useState } from "react";
+import { StateContext } from "../context/State";
 
 function Posts() {
   return (
@@ -7,6 +9,8 @@ function Posts() {
       <div className="container mx-auto my-20">
         <h2 className="text-center my-3 text-2xl text-[#34BAED] font-bold capitalize">SCHOLARSHIP-POSTS</h2>
         <div className="flex gap-12 justify-between items-center">
+          {console.log(scholarships)}
+
           <Post icon={munene} category="Robert Munene" type="fulltimescholarship" requirements=""/>
           <Post icon={munene} category="Duncan Lanogwa" type="fulltimescholarship" requirements="" />
           <Post icon={munene} category="Joyce Mideva" type="fulltimescholarship" requirements=""/>
