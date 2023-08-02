68d97efe1aafaffb63db97efe5bc8af024055825
JoyceMideva <mideva061@gmail.com>
2023-08-02 11:00:51 +0200

    did the add

---
+++ src/context/State.jsx
@@ -0,0 +1,13 @@
+import { useContext,createContext, useState } from "react"
+export const StateContext = createContext();
+
+function State() {
+const [schorlar,setScholar]=useState()
+    return(
+        <div>
+
+        </div>
+
+    )
+}
+export default State
---
+++ src/pages/View.jsx
@@ -0,0 +1,16 @@
+import Footer from "../components/Footer";
+import Nav from "../components/Nav";
+
+function View (){
+    return(
+        <div className="h-[100vh] flex flex-col">
+            <Nav className="flex-1"/>
+            <main className="flex-1">
+
+            <h1>View more Applications </h1>
+            </main>
+            <Footer className="flex-1"/>
+        </div>
+    )
+}
+export default View
